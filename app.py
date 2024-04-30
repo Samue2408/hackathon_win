e08d55a6f3bb50f3514d4629eccdfbb4a2670b66
Mauricio Molina <molinamau37@gmail.com>
2024-04-30 11:24:52 -0500

    addding 404

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from config.db import app, db
 
 from api.Roles import ruta_rol
 from api.Users import ruta_users
+from api.materials import ruta_materials
 
 app.register_blueprint(ruta_rol, url_prefix="/api/roles")
 app.register_blueprint(ruta_users, url_prefix="/api/users")
+app.register_blueprint(ruta_materials, url_prefix="/api/materials")
 
 @app.route("/")
 def index():
@@ -15,9 +17,15 @@ def index():
 def home():
     return render_template('home.html')
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'),404
+# @app.errorhandler(404)
+# def not_found_error(error):
+#     return render_template('404.html'),404
+
+
+@app.route("/404")
+def error4():
+    return render_template('404.html')
+
 
 if __name__ == '__main__': 
     app.run(debug=True, port=5000)

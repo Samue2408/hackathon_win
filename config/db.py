6d431045fa1eb5481e8f88f4cf88a2321dc02067
Samue2408 <samuelmaldonadom@gmail.com>
2024-04-30 07:41:07 -0500

    Primer commit

---
+++ config/db.py
@@ -0,0 +1,17 @@
+#aquì colocamos los comandos para la configuraciòn con la bd de mysql
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+
+# Realizar la conexión
+app.config['SQLALCHEMY_DATABASE_URI']= "mysql+pymysql://root@localhost/bdpractica"
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+app.secret_key = "BDpractica"
+
+#creamos los objetos de transferencias a la bd
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)

26f39da57d9e745af06971f8377486b469d65156
Mauricio Molina <molinamau37@gmail.com>
2024-04-30 10:56:47 -0500

    addding 404

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def index():
 def home():
     return render_template('home.html')
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'),404
+
 if __name__ == '__main__': 
     app.run(debug=True, port=5000)
